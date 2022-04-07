e05aa06015678c8e827e120efe5125ae1b08a3e6
Ariel Kouadio <arielkouadio.d@gmail.com>
2022-04-07 14:49:55 +0000

    initial deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import logo from './logo.png';
+import video from './bg-video.mp4';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="middle-box">
+      <img className="logo" src={logo} alt="waterFront-logo" />
+      <h1 className="middle-box__title">BIENTÔT,</h1>
+
+      <h2 className="middle-box__subtitle">Une nouvelle expérience...</h2>
+      <form action="">
+        <input type="email" name="email" placeholder="Email" />
+        <button type="submit">
+          <span className="text">NOTIFY ME </span>
+        </button>
+      </form>
+      <p>Nous ne partagerons pas votre e-mail avec des tiers.</p>
+
+      {ReactDOM.createPortal(
+        <div className="bg-video">
+          <video className="bg-video__content" autoPlay loop muted>
+            <source src={video} type="video/mp4" />
+          </video>
+        </div>,
+        document.body
+      )}
     </div>
   );
 }
-
-export default App;
